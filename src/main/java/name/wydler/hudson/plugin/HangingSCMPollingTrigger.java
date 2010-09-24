d3593ab05372fe1960c85f3ee94e6bd3f34330e2
FNG <daniel.wydler@gmx.ch>
2010-09-24 14:39:24 +0200

    show found scm poller thread to each job. slightly change the pattern to finde the thread for a poller job

--- src/main/java/name/wydler/hudson/plugin/HangingSCMPollingTrigger.java
+++ src/main/java/name/wydler/hudson/plugin/HangingSCMPollingTrigger.java
@@ -46,6 +46,11 @@ public class HangingSCMPollingTrigger extends ManagementLink {
             throw new RuntimeException("No hanging SCM Polling runner found for job " + job);
         } else {
             thread.interrupt();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         rsp.forwardToPreviousPage(req);
@@ -54,9 +59,9 @@ public class HangingSCMPollingTrigger extends ManagementLink {
 
     //TODO return functionaljava Option to avoid null
     private SCMTrigger.Runner getHangingRunnerForJob(String job) {
-        for (SCMTrigger.Runner runner : getHangingScmTriggerRunners()) {
-            if (runner.getTarget().getName().equals(job)) {
-                return runner;
+        for (RunnerWithThreads runner : getHangingScmTriggerRunners()) {
+            if (runner.getRunner().getTarget().getName().equals(job)) {
+                return runner.getRunner();
             }
         }
         return null;
@@ -66,17 +71,36 @@ public class HangingSCMPollingTrigger extends ManagementLink {
     private Thread getThreadForHangingScmPoller(String job) {
         for (Thread thread : Thread.getAllStackTraces().keySet()) {
             String threadName = thread.getName();
-            if (threadName.startsWith("SCM polling for") && threadName.endsWith("[" + job + "]")) {
+            if (threadName.startsWith("SCM polling for") && threadName.contains(job)) {
                 return thread;
             }
         }
         return null;
     }
 
+    private List<Thread> getThreadsForHangingScmRunner(String job) {
+        ArrayList<Thread> threads = new ArrayList<Thread>();
+        for (Thread thread : Thread.getAllStackTraces().keySet()) {
+            String threadName = thread.getName();
+            if (threadName.startsWith("SCM polling for") && threadName.contains(job)) {
+                threads.add(thread);
+            }
+        }
+        return threads;
+    }
 
-    public List<SCMTrigger.Runner> getHangingScmTriggerRunners() {
+
+
+    public List<RunnerWithThreads> getHangingScmTriggerRunners() {
         SCMTrigger.DescriptorImpl scmTriggerDescriptor = (SCMTrigger.DescriptorImpl) SCMTrigger.all().find(SCMTrigger.class);
-        return scmTriggerDescriptor.getRunners();
+        List<SCMTrigger.Runner> runners = scmTriggerDescriptor.getRunners();
+
+        ArrayList<RunnerWithThreads> runnerWithThreadsList = new ArrayList<RunnerWithThreads>();
+        for (SCMTrigger.Runner runner : runners) {
+            String job = runner.getTarget().getName();
+            runnerWithThreadsList.add(new RunnerWithThreads(runner, getThreadsForHangingScmRunner(job)));
+        }
+        return runnerWithThreadsList;
     }
 
     //is called to map to any url below hangingScmPollingTriggers
@@ -84,5 +108,23 @@ public class HangingSCMPollingTrigger extends ManagementLink {
     public Object getDynamic(String token, StaplerRequest req, StaplerResponse rsp) {
         System.out.println("HangingSCMPollingTrigger.getDynamic token: " + token);
         return null;
+    }
+
+    public class RunnerWithThreads{
+        private final SCMTrigger.Runner runner;
+        private final List<Thread> threads;
+
+        public RunnerWithThreads(SCMTrigger.Runner runner, List<Thread> threads) {
+            this.runner = runner;
+            this.threads = threads;
+        }
+
+        public SCMTrigger.Runner getRunner() {
+            return runner;
+        }
+
+        public List<Thread> getThreads() {
+            return threads;
+        }
     }
 }
